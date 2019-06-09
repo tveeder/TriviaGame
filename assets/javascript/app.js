ad50c99531bc950593dd92085e971b9b0835edd5
Other <Other@TRVs-MacBook-Air.local>
2019-06-09 14:18:33 -0500

    add style changes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,33 @@
 
+function startGame() {
+    myGameArea.start();
+}
 
-alert("here we go")
+var myGameArea = {
+    canvas: document.createElement("canvas"),
+    start: function () {
+        this.canvas.width = 480;
+        this.canvas.height = 270;
+        this.context = this.canvas.getContext("2d");
+        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+    }
+}
+
+var timeleft = 30;
+
+canvas {
+    border: 1px solid #d3d3d3;
+    background - color: #f1f1f1;
+}
+
+var downloadTimer = setInterval(function () {
+    document.getElementById("countdown").innerHTML = timeleft + " seconds remaining";
+    timeleft -= 1;
+    if (timeleft <= 0) {
+        clearInterval(downloadTimer);
+        document.getElementById("countdown").innerHTML = "Finished"
+    }
+}, 1000);
+
+
+
