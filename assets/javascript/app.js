7e132d5d80d2561ed1d644e58b0d6881883c1c8d
Other <Other@TRVs-MacBook-Air.local>
2019-06-09 20:04:54 -0500

    adding more functionality

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,33 +1,64 @@
 
-function startGame() {
-    myGameArea.start();
-}
+var correct = 0;
+var incorrect = 0;
 
-var myGameArea = {
-    canvas: document.createElement("canvas"),
-    start: function () {
-        this.canvas.width = 480;
-        this.canvas.height = 270;
-        this.context = this.canvas.getContext("2d");
-        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+$("#thebutton").click(function () {
+
+
+    var timeleft = 10;
+
+    var downloadTimer = setInterval(function () {
+        document.getElementById("countdown").innerHTML = timeleft + " seconds remaining";
+        timeleft -= 1;
+        if (timeleft <= 0) {
+            clearInterval(downloadTimer);
+            document.getElementById("countdown").innerHTML = "Time is up!"
+            checkAnswers()
+        }
+    }, 1000);
+
+});
+
+function checkAnswers() {
+
+    var question1 = document.quizform.question1.value;
+    var question2 = document.quizform.question2.value;
+    var question3 = document.quizform.question3.value;
+
+
+    if (question1 === "merchant") {
+        correct++
     }
-}
+    else incorrect++;
 
-var timeleft = 30;
+    if (question2 === "1941") {
 
-canvas {
-    border: 1px solid #d3d3d3;
-    background - color: #f1f1f1;
-}
-
-var downloadTimer = setInterval(function () {
-    document.getElementById("countdown").innerHTML = timeleft + " seconds remaining";
-    timeleft -= 1;
-    if (timeleft <= 0) {
-        clearInterval(downloadTimer);
-        document.getElementById("countdown").innerHTML = "Finished"
+        correct++
     }
-}, 1000);
+    else incorrect++
+
+    if (question3 === "1974") {
+
+        correct++
+    }
+    else incorrect++
+
+
+    alert("number correct is: " + correct);
+
+    alert("number incorrect is: " + incorrect);
+
+    alert("number unanwered: ");
+
+
+
+
+
+    console.log(correct);
+    console.log(question1);
+    console.log(question2);
+}
+
 
 
 
